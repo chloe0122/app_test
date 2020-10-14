3e3ee408c40fc911f8a148cd3c373d6f1e88648a
chloe0122 <44577489+chloe0122@users.noreply.github.com>
2020-10-14 22:38:52 +0900

    Updates

---
+++ precache-manifest.c7442525e53d8cd1316532eb9bdafa11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36c1f251b4fdbfb62c22ea70d935a0d",
+    "url": "/app_test/index.html"
+  },
+  {
+    "revision": "784ca902f418d948cfc4",
+    "url": "/app_test/static/css/main.048b9712.chunk.css"
+  },
+  {
+    "revision": "83f8cd81ee4f56825155",
+    "url": "/app_test/static/js/2.bb483209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app_test/static/js/2.bb483209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784ca902f418d948cfc4",
+    "url": "/app_test/static/js/main.5bb3ae51.chunk.js"
+  },
+  {
+    "revision": "0aa0751014681fa34258",
+    "url": "/app_test/static/js/runtime-main.24e8763f.js"
+  }
+]);
